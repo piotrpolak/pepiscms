f4144684e33816a8b78fafed43aec50e30800c7c
Piotr Polak <piotr@polak.ro>
2018-02-09 23:59:53 +0100

    Fixed vendor path for travis ci

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,9 +6,15 @@ error_reporting(E_ALL | E_STRICT);
 
 $dir = realpath(dirname(__FILE__));
 
+if (file_exists('./vendor/')) {
+    $vendor_path = realpath('./vendor/') . '/';
+} else {
+    $vendor_path = realpath($dir . '/../../../') . '/';
+}
+
 // Path constants
 defined('PROJECT_BASE') OR define('PROJECT_BASE', realpath($dir . '/../') . '/');
-defined('VENDOR_PATH') OR define('VENDOR_PATH', realpath($dir . '/../../..//') . '/');
+defined('VENDOR_PATH') OR define('VENDOR_PATH', $vendor_path);
 defined('SYSTEM_PATH') OR define('SYSTEM_PATH', PROJECT_BASE . 'codeigniter/');
 defined('APPPATH') OR define('APPPATH', PROJECT_BASE . 'pepiscms/application/');
 defined('BASEPATH') OR define('BASEPATH', PROJECT_BASE . 'pepiscms/');
