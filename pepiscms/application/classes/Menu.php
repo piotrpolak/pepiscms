fe6777118f6bc36baf4c1dcad87045491650b0df
Piotr Polak <piotr@polak.ro>
2018-02-12 23:33:41 +0100

    Document library split into separate classes

---
+++ pepiscms/application/classes/Menu.php
@@ -0,0 +1,219 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See license.txt
+ * @link                http://www.polak.ro/
+ */
+
+
+/**
+ * Menu tree representation used by theme template (frontend).
+ * Contains subelements called MenuItems.
+ *
+ * @since 0.2
+ */
+class Menu
+{
+    /** @var array|null */
+    private $children = NULL;
+
+    /**
+     * Returns children elements of the current menu element
+     *
+     * @return array
+     */
+    public function getChildren()
+    {
+        // Initializing children
+        if ($this->children == NULL) {
+            $this->initializeChildren();
+            $menu_items_array = get_instance()->Menu_model->getSubMenu($this->getId(), Dispatcher::getSiteLanguage()->code); // Hardcoded, to be fixed
+            foreach ($menu_items_array as &$item) {
+                $menu_item = new MenuItem();
+                $menu_item->setLabel($item['item_name']);
+                $menu_item->setId($item['item_id']);
+                $menu_item->setParentId($item['parent_item_id']);
+                $menu_item->setRelativeUrl(Menu::getItemUrl($item, Dispatcher::getUriPrefix())); // Hardcoded, to be fixed
+                $menu_item->setParent($this);
+                $this->addChild($menu_item);
+            }
+        }
+
+        return $this->children;
+    }
+
+    /**
+     * Return menu item by ID
+     *
+     * @param MenuItem $url
+     * @return MenuItem
+     */
+    public function getMenuItemByCanonicalAbsoluteUrl($url)
+    {
+        return self::searchForChildByCanonicalAbsoluteUrl($this->getChildren(), $url);
+    }
+
+    /**
+     * Static recursive function that parses all element structure
+     *
+     * @param Menu $children
+     * @param string $url
+     * @return bool|MenuItem
+     */
+    private static function searchForChildByCanonicalAbsoluteUrl($children, $url)
+    {
+        if (!is_array($children) || !(count($children) > 0)) {
+            return FALSE;
+        }
+
+        foreach ($children as $sub_child) {
+
+            /** @var MenuItem $sub_child */
+            if ($sub_child->getCanonicalAbsoluteUrl() == $url) {
+                return $sub_child;
+            }
+
+
+            $sub_child = self::searchForChildByCanonicalAbsoluteUrl($sub_child->getChildren(), $url);
+            if ($sub_child) {
+                return $sub_child;
+            }
+        }
+
+        return FALSE;
+    }
+
+    /**
+     * Compatibility, will always return 0
+     *
+     * @return int
+     */
+    protected function getId()
+    {
+        return 0;
+    }
+
+    /**
+     * Compatibility, will always return FALSE
+     *
+     * @return bool
+     */
+    public function getParent()
+    {
+        return FALSE;
+    }
+
+    /**
+     * Initializes children, to be ready to use
+     * Returns FALSE if children already initialized
+     *
+     * @return bool
+     */
+    private function initializeChildren()
+    {
+        if ($this->children == NULL) {
+            $this->children = array();
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    /**
+     * Sets the children of the current menu element
+     *
+     * @param array $children
+     */
+    public function setChildren($children)
+    {
+        $this->initializeChildren();
+        foreach ($children as $child) {
+            /** @var MenuItem $child */
+            $child->setParent($this);
+            $child->setParentId($this->getId());
+        }
+        $this->children = $children;
+    }
+
+    /**
+     * Adds a child to the current menu element
+     *
+     * @param MenuItem $child
+     */
+    public function addChild(MenuItem $child)
+    {
+        $this->initializeChildren();
+        $child->setParent($this);
+        $child->setParentId($this->getId());
+        $this->children[] = $child;
+    }
+
+    /**
+     * Tells whether the current menu element has children
+     *
+     * @return bool
+     */
+    public function hasChildren()
+    {
+        return count($this->getChildren()) > 0;
+    }
+
+    /**
+     * Reads into the buffer the whole menu
+     *
+     * @return bool
+     */
+    public function preftechChildrenTree()
+    {
+        return self::preftechChildren($this->getChildren());
+    }
+
+    /**
+     * Static recursive function that parses all element structure
+     *
+     * @param Menu $children
+     * @param int|bool $current_id
+     * @return bool
+     */
+    private static function preftechChildren($children, $current_id = FALSE)
+    {
+        if (!(count($children) > 0)) {
+            return TRUE;
+        }
+
+        foreach ($children as $child) {
+            /** @var MenuItem $child */
+            if ($child->getId() == $current_id) {
+                continue;
+            }
+            self::preftechChildren($child->getChildren(), $child->getId());
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Helper method used to normalize URLs
+     *
+     * @param array $item
+     * @param string $language_prefix
+     * @return string
+     */
+    public static function getItemUrl($item, $language_prefix)
+    {
+        if (!$item['page_uri']) {
+            $url = $item['item_uri'];
+        } else {
+            $url = $language_prefix . $item['page_uri'] . '.html';
+        }
+
+        return $url;
+    }
+
+}
---
+++ pepiscms/application/classes/MenuItem.php
@@ -0,0 +1,176 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See license.txt
+ * @link                http://www.polak.ro/
+ */
+
+/**
+ * Menu item representation. Not a menu itself.
+ *
+ * @since        0.2
+ */
+class MenuItem extends Menu
+{
+    /** @var int */
+    private $id;
+    /** @var int */
+    private $parent_id;
+    /** @var string */
+    private $relative_url;
+    /** @var string */
+    private $canonical_url;
+    /** @var string */
+    private $label;
+    /** @var MenuItem */
+    private $parent;
+
+    /**
+     * Returns ID of the current menu item
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets ID of the current menu item
+     *
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Returns ID of the parent menu item
+     *
+     * @return int
+     */
+    public function getParentId()
+    {
+        return $this->parent_id;
+    }
+
+    /**
+     * Sets ID of the parent menu item
+     *
+     * @param int $parent_id
+     */
+    public function setParentId($parent_id)
+    {
+        $this->parent_id = $parent_id;
+    }
+
+    /**
+     * Returns relative URL of the item
+     *
+     * @return string
+     */
+    public function getRelativeUrl()
+    {
+        return $this->relative_url;
+    }
+
+    /**
+     * Sets relative URL of the element
+     *
+     * @param string $relative_url
+     */
+    public function setRelativeUrl($relative_url)
+    {
+        $this->relative_url = $relative_url;
+    }
+
+    /**
+     * Sets canonical absolute URL of the menu element
+     *
+     * @param string $canonical_url
+     */
+    public function setCanonicalAbsoluteUrl($canonical_url)
+    {
+        $this->canonical_url = $canonical_url;
+    }
+
+    /**
+     * Returns absolute canonical URL, if it does not exist, then it is created from relative URL
+     *
+     * @return string
+     */
+    public function getCanonicalAbsoluteUrl()
+    {
+        if ($this->canonical_url === NULL) {
+            $this->setCanonicalAbsoluteUrl(base_url() . $this->getRelativeUrl());
+        }
+        return $this->canonical_url;
+    }
+
+    /**
+     * Returns label of the menu element
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Sets label of the menu element
+     *
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * Returns parent menu element
+     *
+     * @return Menu
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Sets parent element
+     *
+     * @param Menu $parent
+     */
+    public function setParent(Menu $parent)
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * Returns siblings of the current element
+     *
+     * @return array
+     */
+    public function getSiblings()
+    {
+        return $this->getParent()->getChildren();
+    }
+
+    /**
+     * @return array
+     * @deprecated as PepisCMS 1.0.0
+     */
+    public function getSiblinks()
+    {
+        return $this->getParent()->getChildren();
+    }
+}
