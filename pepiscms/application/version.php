ce139006e8dd251b6f89639c9b899fdcbe87b3c6
Piotr Polak <piotr@polak.ro>
2018-02-12 23:57:08 +0100

    Parsing version stability from composer.json

--- pepiscms/application/version.php
+++ pepiscms/application/version.php
@@ -11,7 +11,9 @@
 
 $__release_version = json_decode(file_get_contents(__DIR__.'/../../composer.json'));
 
+$is_stable = strpos($__release_version->version, '-dev') === FALSE;
+
 define('PEPISCMS_VERSION', $__release_version->version);
-define('PEPISCMS_PRODUCTION_RELEASE', TRUE);
+define('PEPISCMS_PRODUCTION_RELEASE', $is_stable);
 
 unset($__release_version);
