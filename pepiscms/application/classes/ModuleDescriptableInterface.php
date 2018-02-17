8b64873d0d517ab868ff222cb65228f8a54f3c49
Piotr Polak <piotr@polak.ro>
2018-02-17 16:07:57 +0100

    Module runner split into multiple classes

---
+++ pepiscms/application/classes/ModuleDescriptableInterface.php
@@ -0,0 +1,106 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See license.txt
+ * @link                http://www.polak.ro/
+ */
+
+/**
+ * Interface used for describing a module
+ *
+ * @since 0.2.0
+ */
+interface ModuleDescriptableInterface
+{
+    /**
+     * Returns module name
+     *
+     * @param $language
+     * @return string
+     */
+    public function getName($language);
+
+    /**
+     * Returns module description
+     *
+     * @param $language
+     * @return string
+     */
+    public function getDescription($language); // TODO Not implemented
+
+    /**
+     * Returns module version
+     *
+     * @return string
+     */
+    public function getVersion(); // TODO Not implemented
+
+    /**
+     * Tells whether the module should be displayed in menu
+     *
+     * @return bool
+     */
+    public function isDisplayedInMenu(); // TODO Not implemented
+
+    /**
+     * Tells whether the module should be displayed in utilities
+     *
+     * @return bool
+     */
+    public function isDisplayedInUtilities(); // TODO Not implemented
+
+    /**
+     * Returns the list of module submenu elements
+     *
+     * @param $language
+     * @return array
+     */
+    public function getAdminSubmenuElements($language);
+
+    /**
+     * Returns the list of module dashboars elements
+     *
+     * @param $language
+     * @return array
+     */
+    public function getAdminUtilitiesElements($language);
+
+    /**
+     * Returns the list of module landing dashboard elements
+     *
+     * @param $language
+     * @return array
+     */
+    public function getAdminDashboardElements($language);
+
+    /**
+     * Returns the array of URLs to be displayed in sitemap
+     *
+     * @return array
+     */
+    public function getSitemapURLs();
+
+    /**
+     * Returns a list of variables that are used to generate module configuration
+     *
+     * @return array
+     */
+    public function getConfigVariables();
+
+    /**
+     * Executed on installation
+     */
+    public function onInstall();
+
+    /**
+     * Executed on uninstall
+     */
+    public function onUninstall();
+}
---
+++ pepiscms/application/classes/ModuleDescriptor.php
@@ -0,0 +1,124 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See license.txt
+ * @link                http://www.polak.ro/
+ */
+
+/**
+ * Abstract class for building module descriptors
+ *
+ * @since 0.2
+ */
+abstract class ModuleDescriptor implements ModuleDescriptableInterface
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName($language)
+    {
+        return 'Untitled Module';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription($language)
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDisplayedInMenu()
+    {
+        return TRUE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDisplayedInUtilities()
+    {
+        return TRUE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAdminSubmenuElements($language)
+    {
+        return FALSE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAdminUtilitiesElements($language)
+    {
+        // NOT IMPLEMENTED YET
+        return FALSE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAdminDashboardElements($language)
+    {
+        return FALSE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onInstall()
+    {
+        return TRUE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onUninstall()
+    {
+        return TRUE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSitemapURLs()
+    {
+        return FALSE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigVariables()
+    {
+        return FALSE;
+    }
+
+}
