397e78cd91168c5b2211b717f04d387c6295c64d
Piotr Polak <piotr@polak.ro>
2018-10-31 15:57:19 +0100

    TravisCI integration tests

---
+++ build/before_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mysql -e 'create database pepiscms;' && \
+    sudo apt-get update && \
+    sudo apt-get install apache2 libapache2-mod-fastcgi && \
+    # enable php-fpm
+    sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf && \
+    sudo a2enmod rewrite actions fastcgi alias && \
+    echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini && \
+    sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars && \
+    sudo chown -R travis:travis /var/lib/apache2/fastcgi && \
+    ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm && \
+    # configure apache virtual hosts
+    sudo cp -f build/travis-ci-apache /etc/apache2/sites-available/000-default.conf && \
+    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)/app?g" --in-place /etc/apache2/sites-available/000-default.conf && \
+    sudo service apache2 restart && \
+    mkdir app && cd app && cp -a ../vendor . && mkdir -p ./vendor/piotrpolak/pepiscms && cp -a ../pepiscms ./vendor/piotrpolak/pepiscms/ && cp -a ../composer.json ./vendor/piotrpolak/pepiscms/ && cp -a ../build/travis-composer.json ./composer.json && \
+    cp vendor/piotrpolak/pepiscms/pepiscms/resources/config_template/template_index.php ./index.php && \
+    sed -i -e 's/TEMPLATE_VENDOR_PATH/\.\/vendor\//g' ./index.php && \
+    cp vendor/piotrpolak/pepiscms/pepiscms/resources/config_template/template_.htaccess ./.htaccess && \
+    composer install --no-dev  && \
+    rm -rf vendor/piotrpolak/pepiscms/pepiscms/ && cp -a ../pepiscms ./vendor/piotrpolak/pepiscms/  && \
+    php index.php tools install  && \
+    php index.php tools register_admin $PEPIS_CMS_AUTH_EMAIL $PEPIS_CMS_AUTH_PASSWORD  && \
+    chmod 0777 -R application/cache/ application/logs/ && \
+    curl http://localhost/ -s -f -o /dev/null || (echo "Apache test setup is down." && exit 1)
