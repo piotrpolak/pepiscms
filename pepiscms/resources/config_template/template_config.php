13a6ffea683adede46575ebffc9532ea181437bb
Piotr Polak <piotr@polak.ro>
2018-11-02 23:26:10 +0100

    Added PEPIS_CMS_OBJECT_CACHE_OBJECT_IS_ENABLED installation option

--- pepiscms/resources/config_template/template_config.php
+++ pepiscms/resources/config_template/template_config.php
@@ -8,3 +8,14 @@
 |--------------------------------------------------------------------------
 */
 $config['enabled_languages'] = array('english', 'polish');
+
+/*
+|--------------------------------------------------------------------------
+| Enables / disables object cache
+|--------------------------------------------------------------------------
+|
+| Object cache significantly improves system performance but might cause
+| problem in cloud environments.
+|
+*/
+$config['cache_object_is_enabled'] = TEMPLATE_OBJECT_CACHE_OBJECT_IS_ENABLED;
