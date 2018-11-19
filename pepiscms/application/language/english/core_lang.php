8cab640211de679d1fc32936df65a6431f505f13
Piotr Polak <piotr@polak.ro>
2018-11-20 00:14:58 +0100

    Implemented widgets on admin dashboard

--- pepiscms/application/language/english/core_lang.php
+++ pepiscms/application/language/english/core_lang.php
@@ -3,7 +3,6 @@
 
 $lang['about_label_theme_preview']                  = 'Theme preview';
 $lang['dashboard_group_default']                    = 'Shortcuts';
-$lang['dashboard_info']                             = 'This is the default landing page.';
 $lang['dashboard_system_status']                    = 'System status';
 $lang['dashboard_test_error_cache_not_writeable']   = 'The cache directory is not writeable.';
 $lang['dashboard_test_error_development_release']   = 'You are running a development release of PepisCMS. Please do not forget to upgrade.';
--- pepiscms/application/views/admin/about_dashboard.php
+++ pepiscms/application/views/admin/about_dashboard.php
@@ -1,43 +1,45 @@
 <?= display_breadcrumb(array(admin_url() . 'about/dashboard' => $this->lang->line('dashboard_welcome_to') . ' ' . $this->config->item('site_name')), 'pepiscms/theme/img/about/dashboard_32.png') ?>
-<p><?= $this->lang->line('dashboard_info') ?></p>
-
 <?= display_session_message() ?>
 
-<div id="one_pane_utilities_layout">
-    <?php if( count($dashboard_elements_grouped)): ?>
-        <?php foreach($dashboard_elements_grouped as $group => $dashboard_elements): ?>
-            <h1 class="contrasted"><?= $lang->line($group) ?></h1>
-            <ul class="dashboard_actions clear">
-                <?php foreach($dashboard_elements as $dashboard_element): ?>
-                    <?=dashboard_box(
-                        $dashboard_element['label'],
-                        (isset($dashboard_element['url']) && $dashboard_element['url'] ? $dashboard_element['url'] :
-                            ((isset($dashboard_element['module']) && $dashboard_element['module']) ? module_url($dashboard_element['controller']).$dashboard_element['method'] :
-                                admin_url().$dashboard_element['controller'].'/'.$dashboard_element['method'])),
-                        isset($dashboard_element['icon_url']) && $dashboard_element['icon_url'] ? $dashboard_element['icon_url'] : (isset($dashboard_element['controller']) ? module_icon_url($dashboard_element['controller']) : FALSE),
-                        isset($dashboard_element['description']) ? $dashboard_element['description'] : FALSE,
-                        isset($dashboard_element['is_popup']) ? $dashboard_element['is_popup'] : FALSE,
-                        isset($dashboard_element['target']) ? $dashboard_element['target'] : FALSE
-                    )?>
-                <?php endforeach; ?>
-            </ul>
-        <?php endforeach; ?>
-    <?php endif; ?>
+<?php if(count($widgets)): ?>
+<div id="two_pane_layout">
+    <div class="left_option_pane">
+            <?php foreach($widgets as $widget): if(!$widget['show_in_side_pane']) continue?>
+                <h1 class="contrasted"><?= $widget['label'] ?></h1>
+                <?=$widget['widget_html']?>
+            <?php endforeach ?>
+    </div>
 
-    <div class="clear"></div>
+    <div class="right_content_pane">
+<?php endif; ?>
+        <?php if( count($dashboard_elements_grouped)): ?>
+            <?php foreach($dashboard_elements_grouped as $group => $dashboard_elements): ?>
+                <h1 class="contrasted"><?= $lang->line($group) ?></h1>
+                <ul class="dashboard_actions">
+                    <?php foreach($dashboard_elements as $dashboard_element): ?>
+                        <?=dashboard_box(
+                            $dashboard_element['label'],
+                            (isset($dashboard_element['url']) && $dashboard_element['url'] ? $dashboard_element['url'] :
+                                ((isset($dashboard_element['module']) && $dashboard_element['module']) ? module_url($dashboard_element['controller']).$dashboard_element['method'] :
+                                    admin_url().$dashboard_element['controller'].'/'.$dashboard_element['method'])),
+                            isset($dashboard_element['icon_url']) && $dashboard_element['icon_url'] ? $dashboard_element['icon_url'] : (isset($dashboard_element['controller']) ? module_icon_url($dashboard_element['controller']) : FALSE),
+                            isset($dashboard_element['description']) ? $dashboard_element['description'] : FALSE,
+                            isset($dashboard_element['is_popup']) ? $dashboard_element['is_popup'] : FALSE,
+                            isset($dashboard_element['target']) ? $dashboard_element['target'] : FALSE
+                        )?>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endforeach; ?>
+        <?php endif; ?>
 
-    <?php if(count($failed_configuration_tests)): ?>
-    <h1 class="contrasted"><?= $lang->line('dashboard_system_status') ?></h1>
-    <?php foreach( $failed_configuration_tests as $test_name ): ?>
-        <?= display_warning($lang->line('dashboard_test_'.$test_name)) ?>
-    <?php endforeach ?>
-    <?php endif; ?>
+        <?php if(count($failed_configuration_tests)): ?>
+            <h1 class="contrasted"><?= $lang->line('dashboard_system_status') ?></h1>
+            <?php foreach( $failed_configuration_tests as $test_name ): ?>
+                <?= display_warning($lang->line('dashboard_test_'.$test_name)) ?>
+            <?php endforeach ?>
+        <?php endif; ?>
 
-    <?php if(count($widgets)): ?>
-    <div class="clear"></div>
-    <?php foreach($widgets as $widget): ?>
-        <h1 class="contrasted"><?= $widget['label'] ?></h1>
-        <?=$widget['widget_html']?>
-    <?php endforeach ?>
-    <?php endif; ?>
+<?php if(count($widgets)): ?>
+    </div>
 </div>
+<?php endif; ?>
