13c9542e375afc7bd19bf799c8d2a00c28270bca
Piotr Polak <piotr@polak.ro>
2018-11-20 00:10:21 +0100

    Implemented system_info widgets

---
+++ pepiscms/modules/system_info/controllers/System_infoWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See LICENSE.txt
+ * @link                http://www.polak.ro/
+ */
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class System_infoWidget extends Widget
+{
+    public function disk_usage()
+    {
+        $this->load->library('Google_chart_helper');
+        return $this->assign('free_space', $this->System_info_model->getFreeSpace())
+            ->assign('occupied_space', $this->System_info_model->getOccupiedSpace())
+            ->display('disk_usage');
+    }
+}
---
+++ pepiscms/modules/system_info/models/System_info_model.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * PepisCMS
+ *
+ * Simple content management system
+ *
+ * @package             PepisCMS
+ * @author              Piotr Polak
+ * @copyright           Copyright (c) 2007-2018, Piotr Polak
+ * @license             See LICENSE.txt
+ * @link                http://www.polak.ro/
+ */
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class System_info_model extends PEPISCMS_Model
+{
+    public function getFreeSpace()
+    {
+        return disk_free_space(INSTALLATIONPATH);
+    }
+
+    public function getOccupiedSpace()
+    {
+        $this->load->helper('file');
+        $files = get_filenames(INSTALLATIONPATH, TRUE);
+        $filesize_all = 0;
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                $filesize_all += filesize($file);
+            }
+        }
+
+        return $filesize_all;
+    }
+}
