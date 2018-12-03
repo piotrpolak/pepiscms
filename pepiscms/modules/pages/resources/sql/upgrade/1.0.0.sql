25edd4c753984047a97d9c159f58511b0a7dec2f
Piotr Polak <piotr@polak.ro>
2018-12-03 22:32:10 +0100

    Database schema improvements

--- pepiscms/modules/pages/resources/sql/upgrade/1.0.0.sql
+++ pepiscms/modules/pages/resources/sql/upgrade/1.0.0.sql
@@ -2,6 +2,4 @@ SET FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE cms_menu CHANGE `item_url` `item_url` VARCHAR(512)  NOT NULL DEFAULT '' COMMENT 'Used only when page_id is NULL';
 
-DROP TABLE IF EXISTS cms_remote_applications;
-
 SET FOREIGN_KEY_CHECKS=1;
