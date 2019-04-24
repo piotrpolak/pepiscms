159ec740d42467dcc487e9846de3e51c4a7308f8
Piotr Polak <piotr@polak.ro>
2019-04-24 22:05:48 +0200

    Fixed current_url() helper when site running on 443 port

--- pepiscms/application/helpers/PEPISCMS_url_helper.php
+++ pepiscms/application/helpers/PEPISCMS_url_helper.php
@@ -168,10 +168,10 @@ if (!function_exists('current_url')) {
             $pageURL .= 's';
         }
         $pageURL .= '://';
-        if ($_SERVER['SERVER_PORT'] != '80') {
-            $pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-        } else {
+        if ($_SERVER['SERVER_PORT'] == '80' || $_SERVER['SERVER_PORT'] == '443') {
             $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        } else {
+            $pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
         }
         return $pageURL;
     }
