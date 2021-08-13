d606eb22a6e8f72dc0b8bc1d11e6bf6b84f5c3e6
SQ5RWU <qyonek@gmail.com>
2021-08-13 16:58:31 +0200

    Allow user modules resources to be bridged too

--- pepiscms/application/tools/bridge.php
+++ pepiscms/application/tools/bridge.php
@@ -19,7 +19,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * V0.4
  */
 
-$bridged_path = $_GET['bridge'];
+$bridged_path = str_replace('..', '', $_GET['bridge']);
 
 $mime = array(
     'css' => 'text/css',
@@ -63,6 +63,16 @@ if (!$ext) {
 $slash_pos = strpos($bridged_path, "/");
 if ($slash_pos == false) {
     bridge_show_not_found();
+}
+
+$bridged_path_parts = explode('/', $bridged_path);
+
+if ($bridged_path_parts[0] == 'usermodules' && $bridged_path_parts[2] == 'resources'){
+    $user_module_name = $bridged_path_parts[1];
+    $user_module_resources_directory = INSTALLATIONPATH . 'modules/' . $user_module_name .'/resources/';
+    if (is_dir($user_module_resources_directory)){
+        $section_base_paths['usermodules/'.$user_module_name.'/resources/'] = $user_module_resources_directory;
+    }
 }
 
 $destination_file_path = false;
